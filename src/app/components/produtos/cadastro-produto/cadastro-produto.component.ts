2b271cbf58d4da4355ad4ad8b146163bfa194808
Jaisson Caue Pravato <cauepravato5138@gmail.com>
2025-04-28 21:43:25 -0300

    Adicionado produtos e cadastro produto

---
+++ src/app/components/produtos/cadastro-produto/cadastro-produto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-produto',
+  imports: [],
+  templateUrl: './cadastro-produto.component.html',
+  styleUrl: './cadastro-produto.component.css'
+})
+export class CadastroProdutoComponent {
+
+}
