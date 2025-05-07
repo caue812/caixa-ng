92ca7be8ccf3629f00cd993a6ad7c67d474084ab
Jaisson Caue Pravato <cauepravato5138@gmail.com>
2025-05-07 20:54:27 -0300

    Finalizado busca e verificações

--- src/app/components/clientes/cliente-cadastro/cliente-cadastro.component.ts
+++ src/app/components/clientes/cliente-cadastro/cliente-cadastro.component.ts
@@ -20,8 +20,34 @@ export class ClienteCadastroComponent {
 
 
   salvar() {
-    this.salvarEvento.emit();
+    if(this.isFormValid() == false)
+      return;
 
+    this.salvarEvento.emit();
   }
 
-}
+  private isFormValid(){
+    if (this.cliente?.nome.trim() == ""){
+      alert("Nome deve ser preenchido")
+      return false;
+    }
+
+    if(this.cliente?.nome.trim().length! < 3){
+      alert("Nome deve conter no mínimo 3 caracteres")
+      return false;
+    }
+
+    if(this.cliente?.cpf.trim() == ""){
+      alert("CPF deve ser preechido")
+      return false;
+    }
+
+    const regexCpf = /^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/
+
+    if  (regexCpf.test(this.cliente?.cpf!) == false){
+      alert("CPF deve ser preenchido no formato 000.000.000-00")
+      return false;
+    }
+    return true
+  }
+}  
