8540e8610220e9484419055107093afad1da70bb
Jaisson Cauê Pravato <75955@PROWAY.TREINA>
2025-05-07 21:48:46 -0300

    Arrumado o lugar da pasta estoque estoque

--- src/app/components/estoque/estoque/estoque.component.spec.ts
+++ src/app/components/estoque/estoque/estoque.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EstoqueComponent } from './estoque.component';
+import { EstoqueComponent } from '../estoque.component';
 
 describe('EstoqueComponent', () => {
   let component: EstoqueComponent;
